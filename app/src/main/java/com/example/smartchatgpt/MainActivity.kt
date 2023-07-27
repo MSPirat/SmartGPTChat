31ce8f5e95e9b060954a47a45a8b7b5daf93afc2
[Sergey Melnichenko] <[m1s3@mail.ru]>
2023-07-27 15:56:02 +0600

    Update to version model gpt-3.5-turbo, increased query time, added the ability to copy a reply

--- app/src/main/java/com/example/smartchatgpt/MainActivity.kt
+++ app/src/main/java/com/example/smartchatgpt/MainActivity.kt
@@ -13,9 +13,11 @@ import androidx.recyclerview.widget.RecyclerView
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
@@ -25,7 +27,9 @@ class MainActivity : AppCompatActivity() {
     private var messageList = ArrayList<Message>()
     private lateinit var messageAdapter: MessageAdapter
     private val JSON = "application/json; charset=utf-8".toMediaType()
-    private val client = OkHttpClient()
+    private val client = OkHttpClient.Builder()
+        .readTimeout(60, TimeUnit.SECONDS)
+        .build()
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,19 +80,24 @@ class MainActivity : AppCompatActivity() {
         messageList.add(Message(getString(R.string.wait_typing), Message.SEND_BY_BOT))
         val jsonBody = JSONObject()
         try {
-            jsonBody.put("model", "text-davinci-003")
-            jsonBody.put("prompt", question)
-            jsonBody.put("max_tokens", 4000)
-            jsonBody.put("temperature", 0)
+            jsonBody.put("model", "gpt-3.5-turbo")
+
+            val messageArr = JSONArray()
+            val messageObj = JSONObject()
+            messageObj.put("role", "user")
+            messageObj.put("content", question)
+            messageArr.put(messageObj)
+
+            jsonBody.put("messages", messageArr)
         } catch (e: JSONException) {
             e.printStackTrace()
         }
         val body = jsonBody.toString().toRequestBody(JSON)
         val request = Request.Builder()
-            .url("https://api.openai.com/v1/completions")
+            .url("\n" + "https://api.openai.com/v1/chat/completions")
             .header(
                 "Authorization",
-                "Bearer YOUR_GPT_CHAT_KEY"
+                "Bearer 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"
             ) //TODO write your key here for use App
             .post(body)
             .build()
@@ -98,18 +107,23 @@ class MainActivity : AppCompatActivity() {
                 addResponse("Failed to load response ${e.message}")
             }
 
-            override fun onResponse(call: Call, response: Response) = if (response.isSuccessful) {
-                val jsonObject: JSONObject?
-                try {
-                    jsonObject = JSONObject(response.body!!.string())
-                    val jsonArray = jsonObject.getJSONArray("choices")
-                    val result = jsonArray.getJSONObject(0).getString("text")
-                    addResponse(result.trim())
-                } catch (e: JSONException) {
-                    e.printStackTrace()
+            override fun onResponse(call: Call, response: Response) {
+                val responseBody = response.body?.string()
+                if (response.isSuccessful && responseBody != null) {
+                    try {
+                        val jsonObject = JSONObject(responseBody)
+                        val jsonArray = jsonObject.getJSONArray("choices")
+                        val result = jsonArray.getJSONObject(0)
+                            .getJSONObject("message")
+                            .getString("content")
+                        addResponse(result.trim())
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                } else {
+                    val errorMessage = "Failed to load response ${responseBody ?: response.message}"
+                    addResponse(errorMessage)
                 }
-            } else {
-                addResponse("Failed to load response ${response.body}")
             }
         })
     }
